8ab9401b76a10852684be269bfa6689a75e426c8
MGR70 <mguarderasaplicaciones@gmail.com>
2025-03-14 15:42:56 -0500

    Update registerDebt.js

--- api/registerDebt.js
+++ api/registerDebt.js
@@ -5,14 +5,17 @@ export default async function handler(req, res) {
     const { deudor_id, ganador_id, monto } = req.body;
 
     try {
+      console.log('Conectando a la base de datos...');
+      console.log('Registrando deuda:', { deudor_id, ganador_id, monto });
       const result = await sql`
         INSERT INTO Deudas (deudor_id, ganador_id, monto)
         VALUES (${deudor_id}, ${ganador_id}, ${monto})
         RETURNING *;
       `;
+      console.log('Deuda registrada:', result.rows[0]);
       res.status(200).json(result.rows[0]);
     } catch (error) {
-      console.error(error);
+      console.error('Error al registrar la deuda:', error);
       res.status(500).json({ error: 'Error al registrar la deuda' });
     }
   } else {
