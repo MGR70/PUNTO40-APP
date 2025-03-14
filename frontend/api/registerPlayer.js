ff383a34a55163cf0712555b6bdeb50cc0fbb7af
MGR70 <mguarderasaplicaciones@gmail.com>
2025-03-13 23:51:29 -0500

    Update registerPlayer.js

--- frontend/api/registerPlayer.js
+++ frontend/api/registerPlayer.js
@@ -1,32 +1,28 @@
-import { Pool } from 'pg';
+const { Pool } = require('pg');
 
-export default async function handler(req, res) {
+const pool = new Pool({
+    user: 'neondb_owner',
+    host: 'ep-broad-bush-a85uxie7-pooler.eastus2.azure.neon.tech',
+    database: 'neondb',
+    password: 'npg_RHNaKQD7z4wB',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+module.exports = async (req, res) => {
     if (req.method === 'POST') {
         const { nombre } = req.body;
-
-        // Configuración de la base de datos (reemplaza con tus credenciales de Neon.tech)
-        const pool = new Pool({
-            user: process.env.PGUSER,
-            host: process.env.PGHOST,
-            database: process.env.PGDATABASE,
-            password: process.env.PGPASSWORD,
-            port: process.env.PGPORT,
-            ssl: {
-                rejectUnauthorized: false,
-            },
-        });
-
+        const query = 'INSERT INTO Jugadores (nombre) VALUES ($1) RETURNING *';
         try {
-            const query = 'INSERT INTO Jugadores (nombre) VALUES ($1) RETURNING *';
             const result = await pool.query(query, [nombre]);
             res.status(200).json(result.rows[0]);
         } catch (err) {
             console.error(err);
             res.status(500).json({ message: 'Error al registrar el jugador' });
-        } finally {
-            await pool.end();
         }
     } else {
         res.status(405).json({ message: 'Método no permitido' });
     }
-}
+};
