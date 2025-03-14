8b1fcb9a17983a95b8cbae5b70758103c77e86f0
MGR70 <mguarderasaplicaciones@gmail.com>
2025-03-14 14:13:01 -0500

    Update registerPlayer.js

--- api/registerPlayer.js
+++ api/registerPlayer.js
@@ -1,28 +1,21 @@
-const { Pool } = require('pg');
+import { sql } from '@vercel/postgres';
 
-const pool = new Pool({
-    user: 'neondb_owner',
-    host: 'ep-broad-bush-a85uxie7-pooler.eastus2.azure.neon.tech',
-    database: 'neondb',
-    password: 'npg_RHNaKQD7z4wB',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { nombre } = req.body;
 
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        const { nombre } = req.body;
-        const query = 'INSERT INTO Jugadores (nombre) VALUES ($1) RETURNING *';
-        try {
-            const result = await pool.query(query, [nombre]);
-            res.status(200).json(result.rows[0]);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Error al registrar el jugador' });
-        }
-    } else {
-        res.status(405).json({ message: 'Método no permitido' });
+    try {
+      const result = await sql`
+        INSERT INTO Jugadores (nombre)
+        VALUES (${nombre})
+        RETURNING *;
+      `;
+      res.status(200).json(result.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al registrar el jugador' });
     }
-};
+  } else {
+    res.status(405).json({ error: 'Método no permitido' });
+  }
+}
