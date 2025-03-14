e0501fc273ddd6a02706718644c74a4803c769e7
MGR70 <mguarderasaplicaciones@gmail.com>
2025-03-14 15:43:31 -0500

    Update finalizarJuego.js

--- api/finalizarJuego.js
+++ api/finalizarJuego.js
@@ -3,11 +3,14 @@ import { sql } from '@vercel/postgres';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
+      console.log('Conectando a la base de datos...');
+      console.log('Finalizando juego...');
       await sql`DELETE FROM Deudas;`;
       await sql`DELETE FROM Jugadores;`;
+      console.log('Juego finalizado correctamente');
       res.status(200).json({ message: 'Juego finalizado correctamente' });
     } catch (error) {
-      console.error(error);
+      console.error('Error al finalizar el juego:', error);
       res.status(500).json({ error: 'Error al finalizar el juego' });
     }
   } else {
